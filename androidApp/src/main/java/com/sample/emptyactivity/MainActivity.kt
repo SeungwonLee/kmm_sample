00ca77c4f59f2a0c9544b8db55ac95ce9e2a5e5c
Seung won Lee (Ellen) <swsw.lee@linecorp.com>
2020-09-01 20:46:11 +0900

    added multi-platform codes

--- androidApp/src/main/java/com/sample/emptyactivity/MainActivity.kt
+++ androidApp/src/main/java/com/sample/emptyactivity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.sample.emptyactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import sample.Platform
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.and_textview).text = Platform.name
     }
 }
